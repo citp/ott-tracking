ab4fba3c2c34976f120b13ffd385c36c6ac71a7a
Gunes Acar <gunes.acar@esat.kuleuven.be>
2018-12-06 17:18:13 -0500

    Add command line param to starter.sh

--- roku/starter.sh
+++ roku/starter.sh
@@ -3,4 +3,4 @@
 export TV_IP_ADDR=172.24.1.135
 #export SSLKEYLOGFILE="./data/keys/SSLKEYLOGFILE.txt" && python3 -u ./scrape_channels.py |& tee output.txt
 export LOG_OUT_FILE="data/output.txt"
-export MITMPROXY_SSLKEYLOGFILE="./data/keys/SSLKEYLOGFILE.txt" && stdbuf -oL -eL python3 -u ./scrape_channels.py |& tee $LOG_OUT_FILE
+export MITMPROXY_SSLKEYLOGFILE="./data/keys/SSLKEYLOGFILE.txt" && stdbuf -oL -eL python3 -u ./scrape_channels.py $1 |& tee $LOG_OUT_FILE
