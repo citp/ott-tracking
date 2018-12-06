8f5f13c3de738fa48500d28026e5abc1b4209490
Hooman <hoomanm@princeton.edu>
2018-12-06 17:03:02 -0500

    Small mod to starter.sh

--- roku/starter.sh
+++ roku/starter.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 export TV_IP_ADDR=172.24.1.135
-export SSLKEYLOGFILE="./data/keys/SSLKEYLOGFILE.txt" && python3 -u ./scrape_channels.py |& tee output.txt
+#export SSLKEYLOGFILE="./data/keys/SSLKEYLOGFILE.txt" && python3 -u ./scrape_channels.py |& tee output.txt
+export LOG_OUT_FILE="data/output.txt"
+export MITMPROXY_SSLKEYLOGFILE="./data/keys/SSLKEYLOGFILE.txt" && python3 -u ./scrape_channels.py |& tee $LOG_OUT_FILE
