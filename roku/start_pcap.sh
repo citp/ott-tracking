f72c694ec6919478f61ad6a822cae04298c9bbc0
Gunes Acar <gunes.acar@esat.kuleuven.be>
2018-12-06 18:40:05 -0500

    WIP remove all tcpdump filters.

--- roku/start_pcap.sh
+++ roku/start_pcap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Recommended for allowing non-root users to run tcpdump:
 # https://askubuntu.com/questions/530920/tcpdump-permissions-problem
-tcpdump -v -w "$1".pcap -i wlan0 host $TV_IP_ADDR and not arp
+#tcpdump -v -w "$1".pcap -i wlan0 host $TV_IP_ADDR and not arp
+tcpdump -v -w "$1".pcap -i any and not arp
 
