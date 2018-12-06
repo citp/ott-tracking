599ad15b4aa7fbd9429e40a3d8f074634afb32cb
Gunes Acar <gunes.acar@esat.kuleuven.be>
2018-12-06 18:49:01 -0500

    Fix tcpdump cmd.

--- roku/start_pcap.sh
+++ roku/start_pcap.sh
@@ -2,5 +2,5 @@
 # Recommended for allowing non-root users to run tcpdump:
 # https://askubuntu.com/questions/530920/tcpdump-permissions-problem
 #tcpdump -v -w "$1".pcap -i wlan0 host $TV_IP_ADDR and not arp
-tcpdump -v -w "$1".pcap -i any and not arp
+tcpdump -v -w "$1".pcap -i any not arp
 
