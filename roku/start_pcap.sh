f4e5214b5d2ceb7418d91d0eff403cbc3963b8e0
Hooman <hoomanm@princeton.edu>
2018-12-06 20:25:04 -0500

    Filter PCAPS by MAC address

--- roku/start_pcap.sh
+++ roku/start_pcap.sh
@@ -2,5 +2,7 @@
 # Recommended for allowing non-root users to run tcpdump:
 # https://askubuntu.com/questions/530920/tcpdump-permissions-problem
 #tcpdump -v -w "$1".pcap -i wlan0 host $TV_IP_ADDR and not arp
-tcpdump -v -w "$1".pcap -i any not arp
+
+ETH_MAC_ADDRESS="b8:27:eb:a2:f4:10"
+tcpdump -v -w "$1".pcap -i eth0 ether host $ETH_MAC_ADDRESS and not arp
 
