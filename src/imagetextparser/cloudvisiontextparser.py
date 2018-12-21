4f64e0c790d12835e0c1ee2fac300d6860fb38f9
Arunesh Mathur <amathur@umd.edu>
2018-12-21 03:41:30 -0500

    output entire JSON to file

--- src/imagetextparser/cloudvisiontextparser.py
+++ src/imagetextparser/cloudvisiontextparser.py
@@ -4,6 +4,7 @@ import os
 import json
 import argparse
 import logging
+import io
 
 class FailedToQueryError(Exception):
     '''Cloud API returned non 200 status code'''
@@ -43,19 +44,21 @@ def make_request(img_file, img_file_base64, key):
     r = requests.post(endpoint % key, data=payload % img_file_base64, headers=headers)
 
     if r.status_code == 200:
-        mrjson = json.loads(r.text)
         logger.info('%s successfully queried. Code: %d %s.' % (img_file, r.status_code, requests.status_codes._codes[r.status_code][0]))
-        return mrjson['responses'][0]['fullTextAnnotation']['text']
+        mrjson = json.loads(r.text)
+        return mrjson
     else:
         logger.error('%s failed to query. Code: %d %s.' % (img_file, r.status_code, requests.status_codes._codes[r.status_code][0]))
         raise FailedToQueryError()
 
 
-def compile_text(img_dir, writer, key):
+def compile_text(img_dir, output_dir, key):
     for f in os.listdir(img_dir):
         try:
-            text_content = make_request(f, img_base64(os.path.abspath(os.path.join(img_dir, f))), key)
-            writer.write(f + ',' + repr(text_content.encode('utf8')) + '\n')
+            output_json = make_request(f, img_base64(os.path.abspath(os.path.join(img_dir, f))), key)
+
+            with io.open(os.path.abspath(os.path.join(output_dir, f + '.json')), 'w', encoding='utf-8') as outfile:
+                outfile.write(unicode(json.dumps(output_json, ensure_ascii=False)))
         except:
             logger.exception('Exception while extracting text for %s' % f)
             pass
@@ -64,16 +67,16 @@ def compile_text(img_dir, writer, key):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Gets the text in all the image failes in the given directory using the Google Cloud Vision API')
     parser.add_argument('image_dir', help='Folder containing the images')
-    parser.add_argument('output_file', help='Name of the output CSV file')
+    parser.add_argument('output_dir', help='Name of the output folder where the results will be dumped')
     parser.add_argument('key', help='Google Cloud API Key')
 
     args = parser.parse_args()
 
     img_dir = args.image_dir
     key = args.key
-    output_file = args.output_file
+    output_dir = args.output_dir
 
-    writer = open(output_file, 'w')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    writer.write('file,text\n')
-    compile_text(img_dir, writer, key)
+    compile_text(img_dir, output_dir, key)
