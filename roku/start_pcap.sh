5abb951bdfca4ed6bb5fbda9949ffa10e7600ec2
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-02-07 18:22:12 -0500

    Remove start_pcap.sh, not used anymore.

--- roku/start_pcap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Recommended for allowing non-root users to run tcpdump:
-# https://askubuntu.com/questions/530920/tcpdump-permissions-problem
-sudo -E tcpdump -v -w "$1".pcap -i ${LANIF} ether host $ETH_MAC_ADDRESS and not arp and port not ssh
-
