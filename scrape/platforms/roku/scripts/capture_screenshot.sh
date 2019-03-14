fe8c939b0ec6514092454fdfc57607632cc3c3fa
Hooman <hoomanm@princeton.edu>
2019-03-14 18:50:58 -0400

    iptables only filter traffic originated at/destined to TV IP address

--- scrape/platforms/roku/scripts/capture_screenshot.sh
+++ scrape/platforms/roku/scripts/capture_screenshot.sh
@@ -9,5 +9,5 @@ echo "Continuous screenshot capturing to ${ScreenshotFile}"
 
 ffmpeg -loglevel quiet -i /dev/video0 -vf scale=1280:720,fps=1,eq=brightness=-0.1 -r 1 -hide_banner -updatefirst 1 -y $ScreenshotFile
 
-echo "Screenshot script terminating! If your script is ongoing this might mean that the FFMPEG returned with error!"
+echo "Screenshot script terminating! If your crawl is ongoing this might mean that the FFMPEG returned with error!"
 echo "Check ffmpeg command output for more detail!!"
