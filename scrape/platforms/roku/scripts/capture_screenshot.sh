0605ba42da779dbd4b9f442a2d08049fe4ef8597
Hooman <hoomanm@princeton.edu>
2019-03-15 18:13:32 -0400

    Screenshot capture improvement

--- scrape/platforms/roku/scripts/capture_screenshot.sh
+++ scrape/platforms/roku/scripts/capture_screenshot.sh
@@ -5,9 +5,17 @@ pkill -2 -f ffmpeg
 
 # Capture screenshot continuously and write to same file
 ScreenshotFile=${LogDir}/continuous_screenshot.png
+ScreenshotLogFile=${LogDir}/continuous_screenshot.log
 echo "Continuous screenshot capturing to ${ScreenshotFile}"
 
-ffmpeg -loglevel quiet -i /dev/video0 -vf scale=1280:720,fps=1,eq=brightness=-0.1 -r 1 -hide_banner -updatefirst 1 -y $ScreenshotFile
+while true
+do
+    ffmpeg -i /dev/video0 -vf scale=1280:720,fps=1,eq=brightness=-0.1 -r 1 -hide_banner -updatefirst 1 -y $ScreenshotFile >> $ScreenshotLogFile 2>&1
+
+    echo "Screenshot script stopped!"
+    echo "Retrying"
+    sleep 2s
+done
 
 echo "Screenshot script terminating! If your crawl is ongoing this might mean that the FFMPEG returned with error!"
 echo "Check ffmpeg command output for more detail!!"
