b1cf33cde53be01d84586e8a70232be513df27ad
Hooman <hoomanm@princeton.edu>
2019-03-15 14:28:42 -0400

    Disable IPv6 iptable rules for now!

--- scrape/scripts/iptables.sh
+++ scrape/scripts/iptables.sh
@@ -2,6 +2,6 @@
 sudo -E iptables -t nat -A PREROUTING -i $WLANIF -s $TV_IP_ADDR -p tcp --dport 80 -j REDIRECT --to-port ${MITMPROXY_PORT_NO}
 sudo -E iptables -t nat -A PREROUTING -i $WLANIF -s $TV_IP_ADDR -p tcp --dport 443 -j REDIRECT --to-port ${MITMPROXY_PORT_NO}
 
-sudo -E ip6tables -t nat -A PREROUTING -i $WLANIF -s $TV_IP_ADDR -p tcp --dport 80 -j REDIRECT --to-port ${MITMPROXY_PORT_NO}
-sudo -E ip6tables -t nat -A PREROUTING -i $WLANIF -s $TV_IP_ADDR -p tcp --dport 443 -j REDIRECT --to-port ${MITMPROXY_PORT_NO}
+#sudo -E ip6tables -t nat -A PREROUTING -i $WLANIF -p tcp --dport 80 -j REDIRECT --to-port ${MITMPROXY_PORT_NO}
+#sudo -E ip6tables -t nat -A PREROUTING -i $WLANIF -p tcp --dport 443 -j REDIRECT --to-port ${MITMPROXY_PORT_NO}
 
