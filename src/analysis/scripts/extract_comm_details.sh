9c546b0c6d3e884f4d3d88c3089b0ab5e9a3115c
Hooman <hoomanm@princeton.edu>
2019-03-23 23:23:57 -0400

    updated extract info script

--- src/analysis/scripts/extract_comm_details.sh
+++ src/analysis/scripts/extract_comm_details.sh
@@ -5,12 +5,16 @@ if [ ! -d "$DATA_DIR" ]; then
   echo "Usage: $0 CRAWL_DATA_DIR"
 fi
 
+TMP_DIR="temp"
 PCAP_DIR="$1/pcaps"
 KEY_DIR="$1/keys"
 
 
+#MITM attemps (we search for all x509 certs that have mitmproxy in their name)
+FILTER="x509sat.uTF8String==mitmproxy"
+./extract_fields.sh -w $TMP_DIR -s attemp -i $PCAP_DIR -o $KEY_DIR -f $FILTER -t json -e frame.time_epoch -e ip.src -e ip.dst -e tcp.stream
 #All TLS handshake failures
-FILTER="x509sat.uTF8String == \"mitmproxy\""
-./extract_fields.sh -w temp -s attemp -i $PCAP_DIR -o $KEY_DIR -f $FILTER  -e frame.time_epoch -e ip.dst
-FILTER="ssl and ssl.alert_message.desc==48"
-./extract_fields.sh -w temp -s ssl_fail -i $PCAP_DIR -o $KEY_DIR -f $FILTER  -e frame.time_epoch -e ip.dst
+#Full list: https://tools.ietf.org/html/rfc5246#appendix-A.3
+FILTER="ssl.alert_message.desc==46 or ssl.alert_message.desc==48"
+./extract_fields.sh -w $TMP_DIR -s ssl_fail -i $PCAP_DIR -o $KEY_DIR -f $FILTER -t json  -e frame.time_epoch -e ip.src -e ip.dst -e tcp.stream
+
