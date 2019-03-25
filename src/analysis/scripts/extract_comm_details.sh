8686bf9b89091a1dac56d8684db409ebfa5fdced
Hooman <hoomanm@princeton.edu>
2019-03-25 13:07:58 -0400

    Pcap analysis

--- src/analysis/scripts/extract_comm_details.sh
+++ src/analysis/scripts/extract_comm_details.sh
@@ -20,7 +20,7 @@ SUFFIX="tcp_streams"
 #MITM attemps (we search for all x509 certs that have mitmproxy in their name)
 FORMAT="fields"
 FILTER="x509sat.uTF8String==mitmproxy"
-FIELDS="-e frame.time_epoch -e ip.src -e ip.dst -e tcp.stream"
+FIELDS="-e tcp.stream -e frame.time_epoch -e ip.src -e ip.dst"
 SUFFIX="mitmproxy-attemp"
 ./extract_fields.sh -w $TMP_DIR -s $SUFFIX -i $PCAP_DIR -o $KEY_DIR -f $FILTER -t $FORMAT $FIELDS
 
@@ -28,6 +28,9 @@ SUFFIX="mitmproxy-attemp"
 #Full list: https://tools.ietf.org/html/rfc5246#appendix-A.3
 FORMAT="json"
 FILTER="ssl.alert_message.desc==46 or ssl.alert_message.desc==48"
-FIELDS="-e frame.time_epoch -e ip.src -e ip.dst -e tcp.stream"
+FIELDS="-e tcp.stream -e frame.time_epoch -e ip.src -e ip.dst"
 SUFFIX="ssl_fail"
 ./extract_fields.sh -w $TMP_DIR -s $SUFFIX -i $PCAP_DIR -o $KEY_DIR -f $FILTER -t $FORMAT $FIELDS
+
+#Post processing of files
+./post_process.sh $TMP_DIR
--- src/analysis/scripts/pcap_analysis.py
+++ src/analysis/scripts/pcap_analysis.py
@@ -8,17 +8,50 @@ data_dir = sys.argv[1]
 global_df = None
 for txt_path in glob(join(data_dir, "*.uniq")):
     filename = txt_path.split(sep)[-1]
-    print(txt_path)
+#    print(txt_path)
     channel_name = filename.split("-")[0]
-    print(channel_name)
-    df = pd.read_csv(txt_path, sep=',', encoding='utf-8')
+#    print(channel_name)
+    df = pd.read_csv(txt_path, sep=',', encoding='utf-8', index_col=None)
     df['Channel Name'] = channel_name
+    df['MITM Attemp'] = 0
+    df['SSL Failure'] = 0
     if global_df is None:
         global_df = df
     else:
         global_df = global_df.append(df)
-    #print(len(global_df.index))
-    print(global_df)
+    print(len(global_df.index))
+    #print(global_df)
+
 #find all streams in the list that have mitm in the cert
 
-#
+for txt_path in glob(join(data_dir, "*.pcap.mitmproxy-attemp")):
+    filename = txt_path.split(sep)[-1]
+    channel_name = filename.split("-")[0]
+    df = pd.read_csv(txt_path, sep=',', encoding='utf-8', index_col=None)
+    #df['Channel Name'] = channel_name
+    #df['MITM Attemp'] = 1
+    #print( df.columns.str.lower())
+    for i, row in df.iterrows():
+        for name, value in row.iteritems():
+            if name == 'tcp.stream':
+                tcp_stream = value
+                global_df.loc[(global_df['tcp.stream'] ==  tcp_stream)
+                                 & (global_df['Channel Name'] == channel_name), 'MITM Attemp'] = 1
+                break
+
+#find all tls failure due to invalid cert:
+
+for txt_path in glob(join(data_dir, "*.pcap.ssl_fail")):
+    filename = txt_path.split(sep)[-1]
+    channel_name = filename.split("-")[0]
+    df = pd.read_csv(txt_path, sep=',', encoding='utf-8', index_col=None)
+    #df['Channel Name'] = channel_name
+    #df['MITM Attemp'] = 1
+    #print( df.columns.str.lower())
+    for i, row in df.iterrows():
+        for name, value in row.iteritems():
+            if name == 'tcp.stream':
+                tcp_stream = value
+                global_df.loc[(global_df['tcp.stream'] ==  tcp_stream)
+                                 & (global_df['Channel Name'] == channel_name), 'SSL Failure'] = 1
+                break
