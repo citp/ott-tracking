6ea9868e3b3d57c8cd4afc775dc03eacd3e4142a
Hooman <hoomanm@princeton.edu>
2019-04-26 13:29:49 -0400

    Renaming all old files to readonly!

--- legacy-code/roku/ip_forwarding.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo -E sysctl -w net.ipv4.ip_forward=1
-sudo -E sysctl -w net.ipv6.conf.all.forwarding=1
-sudo -E sysctl -w net.ipv4.conf.all.send_redirects=0
--- legacy-code/roku/start_arpspoof.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo arpspoof -i eth0 -t 10.0.0.13 -r 10.0.0.1
-
