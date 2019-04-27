34c3b124eda95c5c8e96399569716b6712261153
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-04-27 00:02:40 -0400

    Print number of detections.

--- scrape/audio_test.py
+++ scrape/audio_test.py
@@ -4,18 +4,20 @@ from glob import glob
 
 
 def process_wave_files(wave_dir):
+    detected_cnt = 0
     results = {}
-    for wave_path in glob(wave_dir + "/*.wav"):
-        #print("Will process", wave_path)
+    for wave_path in glob(wave_dir + "/audio/*.wav"):
+        # print("Will process", wave_path)
         _ch_id = int(wave_path.split("/")[-1].split("-")[0])
-        _result = audio_recorder.audio_played_second(wave_path, 9)
-        results[_ch_id] = _result
+        results[_ch_id] = audio_recorder.audio_played_second(wave_path, 5)
 
     for ch_id in sorted(results.keys()):
         result = results[ch_id]
         if result != -1:
+            detected_cnt += 1
             print(ch_id, result)
-
+    print("Detected playback in %d of the %d channels via audio." % (
+        detected_cnt, len(results)))
 
 
 if __name__ == '__main__':
