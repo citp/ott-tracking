b8bd4fc1344e35e318351e967f5070cee992162c
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-04-30 16:00:30 -0400

    Make the script compatible with Amazon channel names.

--- scrape/detect_audio.py
+++ scrape/detect_audio.py
@@ -1,21 +1,26 @@
 import sys
 import audio_recorder
 from glob import glob
+from os.path import basename
 
 
 def process_wave_files(wave_dir):
     detected_cnt = 0
     results = {}
     for wave_path in glob(wave_dir + "/audio/*.wav"):
+        wave_name = basename(wave_path)
         # print("Will process", wave_path)
-        _ch_id = int(wave_path.split("/")[-1].split("-")[0])
-        results[_ch_id] = audio_recorder.audio_played_second(wave_path, 5)
+        if "-" in wave_name:
+            wave_name = wave_name.split("-")[0]
+        _ch_name = wave_name.rsplit(".", 1)[0]
 
-    for ch_id in sorted(results.keys()):
-        result = results[ch_id]
+        results[_ch_name] = audio_recorder.audio_played_second(wave_path, 5)
+
+    for _ch_name in sorted(results.keys()):
+        result = results[_ch_name]
         if result != -1:
             detected_cnt += 1
-            print(ch_id, result)
+            print(_ch_name, result)
     print("Detected playback in %d of the %d channels via audio." % (
         detected_cnt, len(results)))
 
