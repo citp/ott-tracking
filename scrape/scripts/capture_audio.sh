8966d8e7112efccce16c78278c81915dafa3f447
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-05-07 22:48:18 -0400

    Start the script in the background, quitely.

--- scrape/scripts/capture_audio.sh
+++ scrape/scripts/capture_audio.sh
@@ -15,7 +15,7 @@ do
     CH_AUDIO_DIR=${AUDIO_ROOT_DIR}/${CHANNEL_ID}
     echo "Continuous audio capturing to ${CH_AUDIO_DIR}"
 
-    arecord -t wav -c 2 -f S16_LE -r44100 -d 1 -D hw:2,0 --use-strftime ${CH_AUDIO_DIR}/${CHANNEL_ID}_"%Y%m%d-%H%M%S.wav"
+    arecord -q -t wav -c 2 -f S16_LE -r44100 -d 1 -D hw:2,0 --use-strftime ${CH_AUDIO_DIR}/${CHANNEL_ID}_"%Y%m%d-%H%M%S.wav"
     sox $(ls ${CH_AUDIO_DIR}/${CHANNEL_ID}_*-*.wav | sort -n | tail -n ${DETECTION_WINDOW}) ${CH_AUDIO_DIR}/${CHANNEL_ID}_most_recent.wav
 done
 
